e3cca630429cb0fc85e2fcb3c96e37437c53f87f
Alexander Cooper <alexanderecooper@gmail.com>
2024-06-27 14:53:14 -0700

    fixed material movement

--- raw_materials/raw_material.gd
+++ raw_materials/raw_material.gd
@@ -2,9 +2,12 @@ class_name RawMaterial extends AnimatableBody2D
 
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sensor: Area2D = $Sensor
 
-const MATERIALS_COLLISION_LAYER = 3
-const UNDERGROUND_COLLISION_LAYER = 5
+static var MATERIAL_SIZE = 16
+
+static var MATERIALS_COLLISION_LAYER = 3
+static var UNDERGROUND_COLLISION_LAYER = 5
 
 var at_exit_node: bool = false
 var mock_follow_node: PathFollow2D
@@ -40,26 +43,33 @@ func get_material_id() -> int:
 func try_move(speed: float) -> bool:
 	mock_follow_node.progress += speed
 	var velocity := (mock_follow_node.global_position - global_position).normalized() * speed
+	_align_sensor(velocity)
 	var collision := move_and_collide(velocity, true)
-	if collision == null or not _is_collision_blocking(collision, velocity):
+	if collision == null or not _is_collision_blocking(collision):
 		move_and_collide(velocity)
 		is_moving = true
 		return true
-	is_moving = false
-	return false
+	else:
+		mock_follow_node.progress -= speed
+		is_moving = false
+		return false
 
 
-func _is_collision_blocking(collision: KinematicCollision2D, velocity: Vector2) -> bool:
-	return true # TODO fix this function
-	#var cpos := collision.get_position().normalized()
-	#var angle := velocity.normalized().dot(cpos)
-	#return angle >= 0.5 and angle <= 1.0
-	#
-	#var cpos := collision.get_position()
-	#var direction := velocity.normalized()
-	#var mpos := (global_position + direction * 8)
-	#var rect := Rect2(mpos, direction * 16)
-	#return rect.has_point(cpos)
+func _align_sensor(velocity: Vector2):
+	if abs(velocity.x) > abs(velocity.y):
+		if velocity.x < 0:
+			sensor.rotation = PI
+		else:
+			sensor.rotation = 0
+	else:
+		if velocity.y < 0:
+			sensor.rotation = 3 * PI / 2
+		else:
+			sensor.rotation = PI / 2
+
+
+func _is_collision_blocking(collision: KinematicCollision2D) -> bool:
+	return collision.get_collider() in sensor.get_overlapping_bodies()
 
 
 func start_tunnel():
