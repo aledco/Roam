162e615063be220422ce19069a9a97d0e26b83bb
Alexander Cooper <alexanderecooper@gmail.com>
2024-08-17 17:15:47 -0700

    added max length to tunnel

--- structures/conveyors/tunnel/out/placeholder/tunnel_out_placeholder.gd
+++ structures/conveyors/tunnel/out/placeholder/tunnel_out_placeholder.gd
@@ -1,5 +1,7 @@
 class_name TunnelOutPlaceholder extends ConveyorPlaceholder
 
+const MAX_TUNNEL_DISTANCE = 5
+
 var tunnel_node: Node2D
 var tunnel_in: TunnelIn
 
@@ -38,20 +40,26 @@ func calculate_position():
 		grid_position = (tunnel_in_grid_position * mult + Vector2(tunnel_in.direction * 32)) + offset
 		var index = Vector2i(grid_position / 32)
 		var i = 32
-		while not structure_manager.can_place_structure(index, get_grid_size()):
-			grid_position = (tunnel_in_grid_position * mult + Vector2(tunnel_in.direction * i)) + offset
-			index = Vector2i(grid_position / 32)
-			i += 32
+		grid_position = (tunnel_in_grid_position * mult + Vector2(tunnel_in.direction * i)) + offset
 	else:
 		grid_position = grid_position * mult + offset
 	var grid_index = Vector2i(grid_position / 32)
-	if structure_manager.can_place_structure(grid_index, get_grid_size()):
-		position = StructureManager.get_structure_position(grid_position, get_grid_size(), direction)
+	_is_valid = structure_manager.can_place_structure(grid_index, get_grid_size())
+	position = StructureManager.get_structure_position(grid_position, get_grid_size(), direction)
 
 
 func _process(delta):
 	calculate_position()
-
+	_set_valid_overlay()
 
 func _sum_vec(vec: Vector2):
 	return vec.x + vec.y
+
+func is_valid():
+	if not super.is_valid():
+		return false
+	
+	var grid_index = get_grid_index()
+	var tunnel_in_index = tunnel_in.get_grid_index()
+	var distance = Vector2(grid_index).distance_to(Vector2(tunnel_in_index))
+	return distance <= MAX_TUNNEL_DISTANCE
